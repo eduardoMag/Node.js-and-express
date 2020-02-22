2894c8b7f85ec3194680e0a6617ea6b33e7f43e3
eduardoMag <eduardobautista13@gmail.com>
2020-02-21 23:13:49 -0600

    adding middleware

--- chapter 3/proyect_main/meadowlark.js
+++ chapter 3/proyect_main/meadowlark.js
@@ -1,13 +1,18 @@
 var express = require('express');
 var app = express();
 app.set('port', process.env.PORT || 3000);
-//adding routes for the home page and about page
+
+//adding static middleware
+app.use(express.static(__dirname+'/public'));
+
+//declaring routes for the home page and about page
 app.get('/', function(req, res){
   res.render('home');
 });
 app.get('/about', function(req, res){
   res.render('about');
 });
+
 //404 catch-all handler (middleware)
 app.use(function(req, res, next){
 res.status(404);
@@ -21,12 +26,13 @@ app.use(function(err, req, res, next){
   res.send('500');
 });
 
-app.listen(app.get('port'), function(){
-  console.log( 'Express started on http://localhost:'+app.get('port')+'; press Ctrl-C to terminate.');
-});
-
 var app =express();
-//set up handlebars viw engine
+
+//set up handlebars view engine
 var handlebars = require('express3-handlebars').create({defaultLayout:'main'});
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
+
+app.listen(app.get('port'), function(){
+  console.log( 'Express started on http://localhost:'+app.get('port')+'; press Ctrl-C to terminate.');
+});
