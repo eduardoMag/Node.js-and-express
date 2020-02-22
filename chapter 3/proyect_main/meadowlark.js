787d3c5c631f8fe9312bc5f389ba58271962c9c6
eduardoMag <eduardobautista13@gmail.com>
2020-02-21 23:09:32 -0600

    completed routes for home and about page

--- chapter 3/proyect_main/meadowlark.js
+++ chapter 3/proyect_main/meadowlark.js
@@ -3,28 +3,30 @@ var app = express();
 app.set('port', process.env.PORT || 3000);
 //adding routes for the home page and about page
 app.get('/', function(req, res){
-  res.type('text/plain');
-  res.send('Meadowlark Travel');
+  res.render('home');
 });
 app.get('/about', function(req, res){
-  res.type('text/plain');
-  res.send('About Meadowlark Travel');
+  res.render('about');
 });
-//custom 404 page
-app.use(function(req, res){
-res.type('text/plain');
+//404 catch-all handler (middleware)
+app.use(function(req, res, next){
 res.status(404);
-res.send('404 - Not Found');
+res.render('404');
 });
 
-//custom 500 page
+//500 error handler (middleware)
 app.use(function(err, req, res, next){
   console.error(err.stack);
-  res.type('text/plain');
   res.status(500);
-  res.send('500 - Server Error');
+  res.send('500');
 });
 
 app.listen(app.get('port'), function(){
   console.log( 'Express started on http://localhost:'+app.get('port')+'; press Ctrl-C to terminate.');
 });
+
+var app =express();
+//set up handlebars viw engine
+var handlebars = require('express3-handlebars').create({defaultLayout:'main'});
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
