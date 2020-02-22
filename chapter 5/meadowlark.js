4b18de4f8980f06132b60f820204ad4262136bf0
eduardoMag <eduardobautista13@gmail.com>
2020-02-22 09:23:55 -0600

    fix typos in inicial code.

--- chapter 5/meadowlark.js
+++ chapter 5/meadowlark.js
@@ -2,7 +2,7 @@ var express = require('express');
 var fortune =require('./lib/fortune.js');
 var app = express();
 
-//set up handlebars view engine
+//handlebars view engine
 var handlebars = require('express3-handlebars').create({ defaultLayout:'main'});
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
@@ -10,13 +10,13 @@ app.set('port', process.env.PORT || 3000);
 
 //static middleware
 app.use(express.static(__dirname + '/public'));
-//new routes for templates
+
+//routes for templates
 app.get('/', function(req, res){
   res.render('home');
 });
 app.get('/about', function(req, res){
   res.render('about',{fortune: fortune.getFortune() } );
-  //the getFortune function is seen in chapter 4
 });
 
 //404 catch-all handler (middleware)
@@ -30,20 +30,6 @@ app.use(function(req, res, next){
   res.status(500);
   res.render('500');
 });
-/*
-//adding routes
-app.get('/', function(req, res){
-  res.type('text/plain');
-  res.send('Meadowlark Travel');
-});
-app.get('/about', function(req, res){
-  res.type('text/plain');
-  res.send('About Meadowlark Travel');
-});
-//custom 404 page
-app.use(function(req, res){
-  res.type('text/plain');
-  res.status(404);
 
 app.listen(app.get('port'), function(){
   console.log('Express started on http://localhost:' + app.get('port')+'; press Ctrl-C to terminate');
