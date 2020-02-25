b1168bf7db2a7b6ebb63a64a17d2cca81aaaf8ce
eduardoMag <eduardobautista13@gmail.com>
2020-02-24 20:23:56 -0600

    fixed minor bugs.

--- chapter 5/meadowlark.js
+++ chapter 5/meadowlark.js
@@ -9,7 +9,7 @@ app.use(function(req, res, next){
   next();
 });
 //handlebars view engine
-var handlebars = require('express3-handlebars').create({ defaultLayout:'main'});
+var handlebars = require('express-handlebars').create({ defaultLayout:'main'});
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
 app.set('port', process.env.PORT || 3000);
