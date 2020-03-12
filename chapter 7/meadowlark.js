66bf908abd23ed78df7858b464f30e04cb80d8bd
eduardoMag <eduardobautista13@gmail.com>
2020-03-12 13:04:55 -0600

    adding missing elements in examples.

--- chapter 7/meadowlark.js
+++ chapter 7/meadowlark.js
@@ -1,21 +1,62 @@
 const express = require('express');
 const fortune =require('./lib/fortune.js');
-const app = express();
 
-//static middleware
-app.use(express.static(__dirname + '/public'));
-app.use(function(req, res, next){
-  res.locals.showTests = app.get('env') !== 'production' && req.query.test === '1';
-  next();
-});
-app.use(require('body-parser')());
+const app = express();
 
 //handlebars view engine
 const handlebars = require('express-handlebars').create({ defaultLayout:'main'});
+
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
+
 app.set('port', process.env.PORT || 3000);
 
+app.use(express.static(__dirname + '/public'));
+
+//set 'showTests' context property if jquery contains test=1
+app.use( (req, res, next) => {
+  res.locals.showTests = app.get('env') !== 'production' && req.query.test === '1';
+  next();
+});
+
+//body-parser
+app.use(require('body-parser') () );
+
+//mocked Weather data
+
+getWeatherData()=>{
+  return{
+    locations: [
+      {
+        name: 'Portland',
+        forcastUrl: 'http://www.wunderground.com/US/OR/Portland.html',
+        iconUrl: 'http://icons-ak.wxug.com/i/c/k/cloudy.gif',
+        weather: 'Overcast',
+        temp: '54.1 F (12.3 C)',
+      },
+      {
+        name: 'Bend',
+        forcastUrl: 'http://www.wunderground.com/US/OR/Bend.html',
+        iconUrl: 'http://icons-ak.wxug.com/i/c/k/partlycloudy.gif',
+        weather: 'Partly Cloudy',
+        temp: '55.0 F (12.8 C)',
+      },
+      {
+        name: 'Manzanita',
+        forcastUrl: 'http://www.wunderground.com/US/OR/Manzanita.html',
+        iconUrl: 'http://icons-ak.wxug.com/i/c/k/rain.gif',
+        weather: 'Light Rain',
+        temp: '55.0 F (12.8 C)',
+      },
+    ],
+  };
+}
+//middlewrae for getWeatherData
+app.use( (req,res, next)=>{
+  if(!res.locals.partials) res.locals.partials ={};
+  res.locals.partials.weather = getWeatherData();
+  next();
+});
 //routes for templates
 app.get('/', (req, res)=>{
   res.render('home');
